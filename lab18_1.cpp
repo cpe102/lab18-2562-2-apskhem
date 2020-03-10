4851b8f70ee3754d85b31610da1f9478c57549f5
Apisit Ritreungroj <apisit_lon@hotmail.com>
2020-03-10 17:50:09 +0700

    Done

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
@@ -8,10 +9,45 @@ struct Rect{
 double overlap(Rect,Rect);
 
 int main(){
+
+	double x,y,w,h;
+	
 	cout << "Please input Rect 1 (x y w h): ";
+	cin >> x >> y >> w >> h;
+	Rect rect1 = {x,y,w,h};
+
 	cout << "Please input Rect 2 (x y w h): ";
+	cin >> x >> y >> w >> h;
+	Rect rect2 = {x,y,w,h};
 	
 	
-	cout << "Overlap area = ";	
+	cout << "Overlap area = " << overlap(rect1, rect2);	
 	return 0;
+}
+
+double overlap(Rect rect1, Rect rect2) {
+    double overlapW, overlapH;
+    
+    // swap X
+    if (rect1.x > rect2.x) {
+        Rect t = rect2;
+        rect2 = rect1;
+        rect1 = t;
+    }
+    
+    if (rect1.x + rect1.w > rect2.x) {
+        overlapW = rect1.x + rect1.w - rect2.x;
+        
+        if (overlapW > rect2.w) overlapW = rect2.w;
+    }
+    else return 0;
+    
+    if (rect1.y + rect1.h > rect2.y) {
+        overlapH = rect1.y + rect1.h - rect2.y;
+        
+        if (overlapH > rect2.h) overlapH = rect2.h;
+    }
+    else return 0;
+
+	return overlapW * overlapH;
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,7 +8,10 @@
 using namespace std;
 
 struct student{
-	//Define struct student with four member (name ,id , gender, gpa);
+	string name;
+	string id;
+	string gender;
+	string gpa;
 };
 
 struct course{
@@ -18,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //Correct this line
+student * findstudent(vector<student> &allstudents,string key){ //Correct this line
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -36,7 +39,7 @@ void printreport(vector<course> allcourses){
 		cout << "\n\nStudents:\t";
 		for(unsigned int j = 0; j < allcourses[i].student_list.size();j++){
 			if(j != 0) cout << "\t\t";
-			cout << setw(15) << left << allcourses[i].student_list[j]->name << "\t";
+			cout << setw(20) << left << allcourses[i].student_list[j]->name << "\t";
 			cout << allcourses[i].student_list[j]->id << "\t";
 			cout << allcourses[i].student_list[j]->gender << "\t";
 			cout << allcourses[i].student_list[j]->gpa << "\n";
@@ -44,6 +47,23 @@ void printreport(vector<course> allcourses){
 		
 	}
 	cout << "-----------------------------------------------------------------------------\n";
+}
+
+vector<string> StrSplit(string t, char splitAt) {
+	vector<string> splitedT;
+	string newT = "";
+	for (unsigned short i = 0; i < t.size(); i++) {
+		if (t[i] == splitAt) {
+			splitedT.push_back(newT);
+			newT = "";
+		}
+		else {
+			newT += t[i];
+		}
+	}
+	if (newT != "") splitedT.push_back(newT);
+	
+	return splitedT;
 }
 
 int main(){
@@ -55,35 +75,38 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-	
-		//Assign value to the members of struct s;
+		student s;
+		
+		vector<string> data = StrSplit(textline, ',');
+
+		s = {data[0], data[1], data[2], data[3]};
 	
 		allstudents.push_back(s); 		
 	}
 	
 	int state = 1;
+	course c;
 	while(getline(course_file,textline)){
 		if(state == 1){
-			course c;
 			int loc = textline.find_first_of('(');
 			c.name = textline.substr(0,loc-1);
 			c.id = atof(textline.substr(loc+1,5).c_str());
 			getline(course_file,textline);
-			allcourses.push_back(c);
 			state = 2;			
 		}else if(state == 2){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-				//Append lecture_list;
+				c.lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
 				state = 1;
+				allcourses.push_back(c);
+				c = {};
 			}else{
-				student *p = findstudent(allstudents,atof(textline.c_str()));
-				//Append student_list;
+				student *p = findstudent(allstudents,textline);
+				c.student_list.push_back(p);
 			}
 		}
 	}
